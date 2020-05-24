4e2f1d56c079f10c2bccd16800cc341d2aa4cea8
Eric Severn <ericsevern@Erics-MacBook-Pro.local>
2020-05-24 13:19:05 -0700

    abstract sekrits

--- delete.py
+++ delete.py
@@ -1,8 +1,22 @@
-# import some pythong thingamajig
-# import requests?
+import yaml
+import twitter
+
+
+# load in twitter sekrits
+with open('tweeter.yaml', 'r') as stream:
+    contents = stream.read()
+    config = yaml.load(contents, Loader=yaml.BaseLoader)
 
 # authenticate tweeter
+api = twitter.Api(
+                consumer_key=config['twitter']['consumer_key'],
+                consumer_secret=config['twitter']['consumer_secret'],
+                access_token_key=config['twitter']['access_token_key'],
+                access_token_secret=config['twitter']['access_token_secret'],
+                sleep_on_rate_limit=True
+                )
 
+print(api.VerifyCredentials())
 # search tweets
 
 
